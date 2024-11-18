40601c29e24734bf572238f8e2923da9c922e0c8
PedroTess <pedrotessniari@gmail.com>
2024-11-18 19:26:48 -0300

    Responsividade finalizada

---
+++ Portifolio/js/scripts.js
@@ -0,0 +1,29 @@
+
+let navBar = document.querySelector('#header')
+
+document.addEventListener("scroll", ()=>{
+    let scrollTop = window.scrollY
+
+    if(scrollTop > 0){
+        navBar.classList.add('rolar')
+    }else{
+        navBar.classList.remove('rolar')
+    }
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const headerOffset = document.querySelector('header').offsetHeight;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    });
+});

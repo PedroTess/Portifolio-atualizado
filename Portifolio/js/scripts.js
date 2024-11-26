9515bf3d69a07f45db0318e8139937d8d580244a
PedroTess <pedrotessniari@gmail.com>
2024-11-26 19:40:07 -0300

    Portifolio sem animações

--- Portifolio/js/scripts.js
+++ Portifolio/js/scripts.js
@@ -27,3 +27,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Menu de navegação mobile
+
+let btnMenuMob = document.querySelector ('#btn-menu-mob')
+let line1 = document.querySelector ('.line-menumob-1')
+let line2 = document.querySelector ('.line-menumob-2')
+let menuMobile = document.querySelector ('#menu-mobile') 
+let body = document.querySelector ('body')
+
+btnMenuMob.addEventListener("click",()=>{
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+    menuMobile.classList.toggle('abrir')
+    body.classList.toggle('no-overflow')
+})
+
+// Fechar o menu mobile ao clicar em um link
+document.querySelectorAll('#menu-mobile a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', () => {
+        // Verifica se o menu está aberto e, se sim, fecha
+        if (menuMobile.classList.contains('abrir')) {
+            line1.classList.remove('ativo1');
+            line2.classList.remove('ativo2');
+            menuMobile.classList.remove('abrir');
+            body.classList.remove('no-overflow');
+        }
+    });
+});
